a242c9e7627ce2bb6ac60103a1cc4a5e099d902e
nguyen315 <hominhnguyen315@gmail.com>
2021-11-23 00:41:52 +0700

    Remove user from class

--- src/components/classroom/UserList/List/ListUsers.js
+++ src/components/classroom/UserList/List/ListUsers.js
@@ -1,8 +1,21 @@
 import React from 'react'
-import { List, ListItem, ListItemAvatar, Avatar, Box } from '@mui/material'
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  Box,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  Button,
+} from '@mui/material'
+import { Delete as DeleteIcon } from '@mui/icons-material'
 import get from 'lodash/get'
 import isEmpty from 'lodash/isEmpty'
 import styled from '@emotion/styled'
+import axiosClient from 'src/axiosClient'
 
 const CustomList = styled(List)`
   max-height: 400px;
@@ -16,19 +29,74 @@ const StatusText = styled('p')`
 `
 
 const ListUsers = ({ users }) => {
+  console.log(users)
+  const [open, setOpen] = React.useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+  const deleteUser = async (user) => {
+    try {
+      await axiosClient.post(
+        `/api/classrooms/${user.classroomId}/remove-user`,
+        {
+          userId: user.userId,
+        }
+      )
+      handleClose()
+    } catch (error) {}
+  }
+
   if (users && !isEmpty(users))
     return (
       <CustomList>
         {users.map((u) => (
-          <ListItem key={u.id}>
-            <Box sx={{ display: 'flex' }}>
-              <ListItemAvatar>
-                <Avatar />
-              </ListItemAvatar>
-              <p>{get(u, 'User.username')}</p>
-            </Box>
-            {u.status === 'PENDING' && <StatusText>({u.status})</StatusText>}
-          </ListItem>
+          <>
+            <ListItem
+              key={u.id}
+              sx={{ display: 'flex', justifyContent: 'space-between' }}
+            >
+              <Box sx={{ display: 'flex' }}>
+                <ListItemAvatar>
+                  <Avatar />
+                </ListItemAvatar>
+                <p>{get(u, 'User.username')}</p>
+                {u.status === 'PENDING' && (
+                  <StatusText>({u.status})</StatusText>
+                )}
+              </Box>
+              <IconButton onClick={handleClickOpen}>
+                <DeleteIcon />
+              </IconButton>
+            </ListItem>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">
+                {'Remove user from classroom'}
+              </DialogTitle>
+              <DialogActions>
+                <Button color="error" onClick={handleClose}>
+                  Cancel
+                </Button>
+                <Button
+                  variant="contained"
+                  color="error"
+                  onClick={() => deleteUser(u)}
+                  autoFocus
+                >
+                  Remove
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </>
         ))}
       </CustomList>
     )
